713ae71c17184f6f722f40889c3d0e7016fb921e
mrPropper911 <67063745+mrPropper911@users.noreply.github.com>
2021-06-22 23:16:14 +0300

    tg_bot-0: add Springboot skeleton to project

---
+++ src/main/java/com/github/javanewsbot/tmfn/TelegramBotForNewsApplication.java
@@ -0,0 +1,13 @@
+package com.github.javanewsbot.tmfn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotForNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotForNewsApplication.class, args);
+	}
+
+}
